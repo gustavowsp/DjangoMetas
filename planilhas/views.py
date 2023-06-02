60afe87887ec0ddfdf8e32ffa884b142fc8895b9
Gustavo Jose Campos <gustavo.campos@mlgomes.com.br>
2023-06-02 16:07:36 -0300

    Algumas att

--- planilhas/views.py
+++ planilhas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import openpyxl
+import pyodbc
+
 
 # Create your views here.
 def index(request):
@@ -8,28 +10,42 @@ def index(request):
 
 
 
-def deflator(request):
+def valor(request):
     
     if request.method == 'POST':
-        print(request.FILES['valopremio'])
-        return render(request, 'planilhas/metaincremento.html')
-    
-    return render(request, 'planilhas/deflatores.html')
+
+        #Verificando se a planilhas existe
+        try:
+            request.FILES['valorpremio'] # Pegando a planilha de valor prêmio
+        except:
+            return render(request, 'planilhas/valorpremio.html')
+
+        #Começando a tratar a planilha
+        planilha = openpyxl.load_workbook(request.FILES['valorpremio'])
+        planilha = planilha['Dados']
+
+        for row in planilha.iter_rows(min_row=4):
+            print(row[0].value)
+
+        
+
+    else:
+        pass
+
+    return render(request, 'planilhas/valorpremio.html')
 
 
 def incremento(request):
 
     if request.method == 'POST':
-        print(request.FILES)
         return render(request, 'planilhas/metaincremento.html')
     
     return render(request, 'planilhas/metaincremento.html')
 
 
-def valor(request):
+def deflator(request):
 
     if request.method == 'POST':
-        print(request.FILES)
         return render(request, 'planilhas/valorpremio.html')
     
     return render(request, 'planilhas/valorpremio.html')
